fd60450a03f418cb46493941ddb9206afcbcb818
robert horsley <roberthorsley@MAC504724.local>
2024-10-09 00:04:34 -0600

    trying to resolve deployment issue in prod

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from 'next/navigation';
 
  
 export default async function Page({ params }: { params: { id: string } }) {
-    const id = await params.id;
+    const id = params.id;
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
