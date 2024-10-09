005e9f6e42443294a1ea8f811f2775917943b088
robert horsley <roberthorsley@MAC504724.local>
2024-10-08 23:20:32 -0600

    correct lingting oversight to allow application to properly deploy

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,9 +56,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // If a database error occurs, return a more specific error.
     return {
-      message: 'Database Error: Failed to Create Invoice.',
+      message: `Database Error: Failed to Create Invoice - message ${error}`,
     };
   }
  
